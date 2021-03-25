e266d6b8c93e90097fa28b9323a2bc6855a2b917
gnadi4 <uno6070w@gmail.com>
2021-03-25 09:03:54 +0900

    pe4

---
+++ ProjectEuler/pe4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //palindrome
+
+    for(int i=999; i>=100; i--){
+        for(int j=999; j>=100; j--){
+            int tmp = i*j;
+            if(tmp<=1000000&&tmp>=)
+        }
+    }
+
+    return 0;
+}
