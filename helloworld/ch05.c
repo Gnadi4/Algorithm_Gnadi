9b576f0e22532f8ad4a446629f58946b3444f9a5
Gnadi4 <uno6070w@gmail.com>
2021-09-14 00:37:33 +0900

    20210914

--- helloworld/ch05.c
+++ helloworld/ch05.c
@@ -32,11 +32,12 @@ int main(){
 
     int arr[3][4] = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
-    int (*ptr)[4] = arr;
-
-    for(int i=0; i<3; i++){
-        for(int j=0; j<4; j++)printf("%d ",ptr[i][j]);
-        printf("\n");
+    int (*pi)[4] = arr;
+    
+    for(; pi<arr+3; pi++){
+        for(int *pj=*pi; pj<*pi+4; pj++){
+            printf("%d ",(*pj));
+        }printf("\n");
     }
 
 }
---
+++ helloworld/ch06.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+typedef unsigned short int age_t;
+
+int main(){
+
+    age_t userAge;
+
+}
